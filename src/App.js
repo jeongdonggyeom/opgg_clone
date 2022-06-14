8008712f56ced05aac83c9c1717440b4fe2c9898
jeongdonggyeom <topcome928@gmail.com>
2022-06-14 14:59:10 +0900

    add: 커뮤니티 미리보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Nav from "./component/Nav"
 import Main from "./component/Main";
+import Community from "./component/Community";
+import './styleComponent/app.css'
 
 function App() {
   return (
-    <div>
+    <div className={"root-div"}>
       <Nav/>
         <Main/>
+        <Community />
     </div>
   );
 }
