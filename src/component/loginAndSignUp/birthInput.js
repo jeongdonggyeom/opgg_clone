51cc42321afbda6ce6922c4b2a811333019f8cad
jeongdonggyeom <topcome928@gmail.com>
2022-08-06 23:06:03 +0900

    커밋

--- src/component/loginAndSignUp/birthInput.js
+++ src/component/loginAndSignUp/birthInput.js
@@ -2,21 +2,51 @@ import { useEffect, useState } from "react";
 
 export default function BirthInput(props)
 {
-    let set = [];
+    let set = ['_', '_', '_', '_', '/', '_', '_', '/', '_', '_'];
+    let index = 0;
+
     const [disable, setDisable] = useState(false);
     const [text, setText] = useState('생년월일 정보는 가입 연령 확인에만 이용됩니다.');
+
     const date = new Date();
     const month = date.getMonth()+1;
     const day = date.getDay() > 9 ? date.getDay() : "0" + date.getDay();
     const today = `생년월일ex(-${date.getFullYear()}/${month > 9 ? month : "0" + month}/${day})`
-    console.log(today);
 
     const input = () => {
         props.func(1);
     }
 
     const inputClick = (e) => {
-        e.target.value = set;
+        let str
+        if(set[index] !== '/' && index <= 10 && index !== 0){
+            set[index++] = e.target.value;
+        }
+        if(set[index] === '/' && index !== 0) {
+            index+=1
+            set[index] = e.target.value;
+        }
+        for(let i=0;i<set.length;i++){
+            str += set[i];
+        }
+        e.target.value = str;
+    }
+
+    const inputBack = (e) => {
+        let str;
+        if(e.key === 8){
+            if(set[index] !== '/' && index <= 10){
+                set[index--] = '_';
+            }
+            if(set[index] === '/'){
+                index-=1;
+                set[index--] = '_';
+            }
+        }
+        for(let i=0;i<set.length;i++){
+            str += set[i];
+        }
+        e.target.value = str;
     }
 
     useEffect(()=>{
@@ -37,7 +67,7 @@ export default function BirthInput(props)
         <div className="birth-root">
             <p className="birth-p">생년월일 입력</p>
             <label>{today}</label>
-            <input type="tel" className="birth-input" onChange={(e)=>{props.setInput(e.target.value)}} value={''} onClick={(e)=>{inputClick(e)}} />
+            <input type="tel" className="birth-input" onChange={(e)=>{props.setInput(e.target.value)}} value={''} onClick={(e)=>{inputClick(e)}} onKeyDown={(e)=>{inputBack(e)}} />
             <span className="birth-span">{text}</span>
             <button className="birth-button" onClick={input} disabled={disable}>다음</button>
             <span className="birth-span2">이미 회원이신가요?</span>
